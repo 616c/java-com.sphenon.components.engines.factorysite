a5e68cef613447cbee3fdf4a02a5d6c232385a8a
Andreas Leue <adev@leue.net>
2018-07-07 17:21:23 +0200

    published from root repository

--- src/generated/java/com/sphenon/engines/factorysite/tplinst/Reference_DataSourceConnector_.java
+++ src/generated/java/com/sphenon/engines/factorysite/tplinst/Reference_DataSourceConnector_.java
@@ -1,18 +1,4 @@
 // instantiated with javainst.pl from /workspace/sphenon/projects/components/basics/reference/v0001/origin/source/java/com/sphenon/basics/reference/templates/Reference.javatpl
-
-/****************************************************************************
-  Copyright 2001-2018 Sphenon GmbH
-
-  Licensed under the Apache License, Version 2.0 (the "License"); you may not
-  use this file except in compliance with the License. You may obtain a copy
-  of the License at http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-  License for the specific language governing permissions and limitations
-  under the License.
-*****************************************************************************/
 // please do not modify this file directly
 package com.sphenon.engines.factorysite.tplinst;
 
@@ -27,6 +13,20 @@ import com.sphenon.basics.metadata.*;
 import com.sphenon.basics.metadata.tplinst.*;
 import com.sphenon.basics.metadata.Type;
 import com.sphenon.basics.many.tplinst.*;
+
+/****************************************************************************
+  Copyright 2001-2018 Sphenon GmbH
+
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+  License for the specific language governing permissions and limitations
+  under the License.
+*****************************************************************************/
 
 import com.sphenon.basics.context.*;
 import com.sphenon.basics.reference.*;
---
+++ src/main/java/com/sphenon/engines/factorysite/test/QuickStart.java
@@ -0,0 +1,58 @@
+package com.sphenon.engines.factorysite.test;
+
+/****************************************************************************
+  Copyright 2001-2018 Sphenon GmbH
+
+  Licensed under the Apache License, Version 2.0 (the "License"); you may not
+  use this file except in compliance with the License. You may obtain a copy
+  of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+  License for the specific language governing permissions and limitations
+  under the License.
+*****************************************************************************/
+
+import com.sphenon.basics.context.*;
+import com.sphenon.basics.configuration.*;
+import com.sphenon.basics.exception.*;
+import com.sphenon.basics.notification.*;
+import com.sphenon.basics.customary.*;
+
+import com.sphenon.engines.factorysite.*;
+import com.sphenon.engines.factorysite.factories.*;
+
+import java.util.Hashtable;
+
+public class QuickStart {
+
+    public static void main(String[] args) {
+
+        // optional: in case you want to provide configuration via command line
+        Configuration.checkCommandLineArgs(args);
+
+        // required: sphenon library needs a context
+        Context context = com.sphenon.basics.context.classes.RootContext.getRootContext();
+        // required: and needs to be initialised
+        Configuration.initialise(context);
+
+        Object result = null;
+
+        try {
+
+            // our test: setting up factory for aggregates
+            Factory_Aggregate fa = new Factory_Aggregate(context);
+            // the aggregate we want to create
+            // (it is found in the jar at the given location, named QuickStart.ocp
+            fa.setAggregateClass(context, "com/sphenon/engines/objectassembler/QuickStart");
+            // and go...
+            result = fa.create(context);
+        } catch (ExceptionError e) {
+            NotificationContext.sendTrace(context, Notifier.CHECKPOINT, "Could not build object aggregate: %(reason)", "reason", e);
+            return;
+        }
+    }
+
+}
+
